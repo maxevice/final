ce61547100dc1f78c88bc3e7cab61b6d4d50de11
Максим Повнич <maxympovnych@gmail.com>
2025-06-02 01:26:24 +0300

    user schema created

---
+++ server/schemas/user.schema.js
@@ -0,0 +1,15 @@
+const registerSchema = {
+  body: {
+    type: 'object',
+    required: ['email', 'password'],
+    properties: {
+      email: { type: 'string', format: 'email' },
+      password: { type: 'string', minLength: 6 }
+    },
+    additionalProperties: false
+  }
+};
+
+module.exports = {
+  registerSchema
+};
