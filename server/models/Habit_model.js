60e346e80be23973dd8b699979fae9a331ad2f3f
Максим Повнич <maxympovnych@gmail.com>
2025-06-03 00:44:48 +0300

    Habit model created

---
+++ server/models/Habit_model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const habitSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Habit = mongoose.model('Habit', habitSchema);
+
+module.exports = Habit;
