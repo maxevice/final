f85f60b2ddd02214e96a58becbef59bfae06a352
Максим Повнич <maxympovnych@gmail.com>
2025-06-03 00:44:18 +0300

    habit routes added

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,27 @@
 const { handleRegister, handleLogin } = require('../controllers/user_controller');
 const { registerSchema } = require('../schemas/user.schema');
+const { getHabits, createHabit } = require('../controllers/habit_controller');
+const { getHabitsSchema, createHabitSchema } = require('../schemas/habit.schema');
+const verifyToken = require('../plugins/auth');
 
 const userRoutes = async (fastify, opts) => {
   fastify.post('/register', { schema: registerSchema }, handleRegister);
   fastify.post('/login', {schema: registerSchema}, handleLogin)
 };
 
-module.exports = userRoutes;
+
+const habitRoutes = async (fastify, opts) => {
+  fastify.get('/', {
+    preHandler: verifyToken,
+    schema: getHabitsSchema
+  }, getHabits);
+
+  fastify.post('/', {
+    preHandler: verifyToken,
+    schema: createHabitSchema
+  }, createHabit);
+};
+
+module.exports = {habitRoutes, userRoutes};
+
+
