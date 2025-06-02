f2a4308985c283b97129f440c15f63f5bb15e054
Максим Повнич <maxympovnych@gmail.com>
2025-06-02 03:46:15 +0300

    login route added

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,9 @@
-const { handleRegister } = require('../controllers/user_controller');
+const { handleRegister, handleLogin } = require('../controllers/user_controller');
 const { registerSchema } = require('../schemas/user.schema');
 
 const userRoutes = async (fastify, opts) => {
   fastify.post('/register', { schema: registerSchema }, handleRegister);
+  fastify.post('/login', {schema: registerSchema}, handleLogin)
 };
 
 module.exports = userRoutes;
