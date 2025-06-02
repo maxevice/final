9d0c664edbbae516d765ada2413265e7023690d2
Максим Повнич <maxympovnych@gmail.com>
2025-06-03 00:45:54 +0300

    habits handler created

---
+++ server/controllers/habit_controller.js
@@ -0,0 +1,27 @@
+const Habit = require('../models/Habit_model');
+
+const createHabit = async (req, reply) => {
+  const { title } = req.body;
+  const userId = req.user.id;
+
+  const newHabit = await Habit.create({
+    title,
+    owner: userId
+  });
+
+  reply.code(201).send(newHabit);
+};
+
+const getHabits = async (req, reply) => {
+  const userId = req.user.id;
+
+  const habits = await Habit.find({ owner: userId }).sort({ createdAt: -1 });
+
+  reply.send(habits);
+};
+
+
+module.exports = {
+  getHabits,
+  createHabit 
+};
