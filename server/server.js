9326fdc852229e39a3200d9e467c18fb6475147d
Максим Повнич <maxympovnych@gmail.com>
2025-06-03 05:38:47 +0300

    habit routes created, route for file transfer created

--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
-const Fastify = require('fastify')
+const Fastify = require('fastify');
+const fastifyStatic = require('@fastify/static');
 const mongoose = require('mongoose')
+const path = require('path');
 require('dotenv').config()
-const userRoutes = require('./routes/routes');
 
-const server = Fastify({
-  logger: true,
-});
+const server = Fastify();
+
+const {userRoutes, habitRoutes} = require('./routes/routes');
 
 server.register(userRoutes, { prefix: '/api/users' });
-
-server.get('/ping', async (req, reply) => {
-  return { hello: 'world' };
+server.register(habitRoutes, { prefix: '/api/habits' });
+server.register(fastifyStatic, {
+  root: path.join(__dirname, '..', 'client'),
+  prefix: '/',
 });
+
 
 const start = async () => {
   try {
+
     await mongoose.connect(process.env.MONGO_URL)
     console.log('✅ MongoDB Atlas підключено');
-
-    await server.listen({ port: 3000});
-    console.log('Сервер запущено на http://localhost:3000');
+    await server.listen({ port: 3000 });
+    console.log('✅ Сервер працює: http://localhost:3000');
   } catch (err) {
-    server.log.error(err);
+    console.error('❌ Помилка при запуску:', err);
     process.exit(1);
   }
 };
