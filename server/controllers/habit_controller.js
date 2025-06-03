53d7f3ba6c4a67d02eac7924094911e3b80839d4
Максим Повнич <maxympovnych@gmail.com>
2025-06-03 05:32:40 +0300

    validation added

--- server/controllers/habit_controller.js
+++ server/controllers/habit_controller.js
@@ -3,7 +3,10 @@ const Habit = require('../models/Habit_model');
 const createHabit = async (req, reply) => {
   const { title } = req.body;
   const userId = req.user.id;
-
+  const exists = await Habit.findOne({ title, owner: userId });
+  if (exists) {
+    return reply.code(400).send({ error: 'Звичка з такою назвою вже існує' });
+  }  
   const newHabit = await Habit.create({
     title,
     owner: userId
